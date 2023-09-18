27f3f942c18dd546dd46149ae6a36a75eb07300d
matin sangabi <sangabiwork@gmail.com>
2023-09-18 09:40:21 +0330

    fix scroll horizontal gif page

--- src/container/layout/index.js
+++ src/container/layout/index.js
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
     <div className="flex h-screen  bg-[#FAFAFB]">
       {/* Desktop sidebar */}
       <Aside />
-      <div className="flex flex-1 flex-col w-full">
+      <div className="flex flex-1 flex-col w-full overflow-hidden">
         {/* header */}
         <Header />
         <main className="h-full flex flex-col gap-y-4 overflow-y-auto overflow-x-hidden  px-3 lg:px-6 py-4 mb-10 pb-20 lg:pb-0 lg:mb-0 ">
